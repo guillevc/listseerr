69da820e5524cd40fb609622e5c52da98c106380
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2025-12-08 23:25:03 +0100

    refactor: replace deprecated eslint function

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
@@ -27,4 +28,4 @@ export default tseslint.config(
       'react-hooks/set-state-in-effect': 'off',
     },
   },
-)
+]
