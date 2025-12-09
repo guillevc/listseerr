afab69f843fe1ecf9e45b842ce1d98458a289c88
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2025-12-09 03:30:31 +0100

    fix: css

--- src/client/components/ui/input.tsx
+++ src/client/components/ui/input.tsx
@@ -9,7 +9,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md px-3 py-2 text-sm disabled:cursor-not-allowed disabled:opacity-50 bg-background placeholder:text-muted border border-input',
+          'flex h-10 w-full rounded-md px-3 py-2 text-sm',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          'bg-background placeholder:text-muted',
+          'border border-input',
+          'outline-none',
+          'focus-visible:ring-2 focus-visible:ring-ring',
           className
         )}
         ref={ref}
