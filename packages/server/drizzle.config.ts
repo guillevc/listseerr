a1a30e5295dd0eeddeee81f40bd8b7790c6dc92a
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2025-12-11 17:26:58 +0100

    refactor: move drizzle file

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from 'drizzle-kit';
-import { env } from './packages/server/src/env';
+import { env } from './src/env';
 
 export default {
-  schema: './packages/server/src/db/schema.ts',
-  out: './packages/server/src/db/migrations',
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
   dialect: 'sqlite',
   dbCredentials: {
     url: env.DATABASE_PATH,
