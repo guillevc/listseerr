247439a9a6a7e77b457018f79676a0b4cecb82c2
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2025-12-12 11:39:10 +0100

    perf(client): visualize bundle size

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    process.env.ANALYZE === 'true' &&
+      visualizer({
+        filename: './stats.html',
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
