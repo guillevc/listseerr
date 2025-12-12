d1b99eb785b7801d4d6e559cc5d04565e75357c6
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2025-12-12 02:33:17 +0100

    chore: remove unnecessary script

--- scripts/git-changed-files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git ls-files -m | while read file; do
-  echo "// ----------------------------------------"
-  echo "// FILE: $file"
-  echo "// ----------------------------------------"
-  cat "$file"
-  echo
-done
